10bbff4968eb3bdffda11c89e54bf5e2eb0af7ef
a-yoshinaga <a.yoshinaga0314@gmail.com>
2016-10-02 20:07:08 +0900

    first
    first

---
+++ IbeaconIOSTests/IbeaconIOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  IbeaconIOSTests.swift
+//  IbeaconIOSTests
+//
+//  Created by YoshinagaAtsushi on 2016/08/20.
+//  Copyright © 2016年 YoshinagaAtsushi. All rights reserved.
+//
+
+import XCTest
+@testable import IbeaconIOS
+
+class IbeaconIOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
